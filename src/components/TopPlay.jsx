984a92d3238f25e3e3bcbb26b81d3e0eb9861b82
zackkb <98679473+zackkb@users.noreply.github.com>
2022-12-13 08:20:33 -0600

    Fixing artists, adding PlayPause functionality

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,7 +11,7 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 
-const TopChartCard = ({ song, i }) => (
+const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
@@ -25,10 +25,17 @@ const TopChartCard = ({ song, i }) => (
         </Link>
       </div>
     </div>
+    <PlayPause
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePause={handlePauseClick}
+      handlePlay={handlePlayClick}
+    />
   </div>
 );
 
-const TopPlay = ({ song, i }) => {
+const TopPlay = () => {
   const dispatch = useDispatch();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data } = useGetTopChartsQuery();
@@ -44,7 +51,7 @@ const TopPlay = ({ song, i }) => {
     dispatch(playPause(false));
   };
 
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
@@ -58,12 +65,17 @@ const TopPlay = ({ song, i }) => {
             <p className="text-gray-300 text-base cursor-pointer">See more</p>
           </Link>
         </div>
+
         <div className="mt-4 flex flex-col gap-1">
           {topPlays?.map((song, i) => (
             <TopChartCard
               key={song.key}
               song={song}
               i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={() => handlePlayClick(song, i)}
             />
           ))}
         </div>
@@ -76,6 +88,7 @@ const TopPlay = ({ song, i }) => {
             <p className="text-gray-300 text-base cursor-pointer">See more</p>
           </Link>
         </div>
+
         <Swiper
           slidesPerView="auto"
           spaceBetween={15}
@@ -85,14 +98,14 @@ const TopPlay = ({ song, i }) => {
           modules={[FreeMode]}
           className="mt-4"
         >
-          {topPlays?.map((song, i) => (
+          {topPlays?.slice(0, 5).map((artist) => (
             <SwiperSlide
-              key={song?.key}
+              key={artist?.key}
               style={{ width: '25%', height: 'auto' }}
               className="shadow-lg rounded-full animate-slideright"
             >
-              <Link to={`/artists/${song?.artists[0].adamid}`}>
-                <img src={song?.images.background} alt="name" className="rounded-full w-full object-cover" />
+              <Link to={`/artists/${artist?.artists[0].adamid}`}>
+                <img src={artist?.images?.background} alt="Name" className="rounded-full w-full object-cover" />
               </Link>
             </SwiperSlide>
           ))}
