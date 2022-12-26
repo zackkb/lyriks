1732d1bac253a5dac3907379a355c19339bdea73
zackkb <98679473+zackkb@users.noreply.github.com>
2022-12-25 21:52:37 -0600

    small changes in topPlay and adding AroundYou component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -12,16 +12,20 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 
 const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
-  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+  <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
       <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
       <div className="flex-1 flex flex-col justify-center mx-3">
         <Link to={`/songs/${song.key}`}>
-          <p className="text-xl font-bold text-white">{song?.title}</p>
+          <p className="text-xl font-bold text-white">
+            {song?.title}
+          </p>
         </Link>
         <Link to={`/artists/${song?.artists[0].adamid}`}>
-          <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
+          <p className="text-base text-gray-300 mt-1">
+            {song?.subtitle}
+          </p>
         </Link>
       </div>
     </div>
@@ -110,7 +114,6 @@ const TopPlay = () => {
             </SwiperSlide>
           ))}
         </Swiper>
-
       </div>
     </div>
   );
--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
+
+const AroundYou = () => {
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(true);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  useEffect(() => {
+
+  }, [country]);
+
+  return (
+    <div />
+  );
+};
 
 export default CountryTracks;
