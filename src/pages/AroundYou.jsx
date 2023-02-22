692fe4f973187119763372d7694e748a8c09aa1b
zackkb <98679473+zackkb@users.noreply.github.com>
2023-02-22 08:34:07 -0600

    small changes

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,31 +1,36 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useSelector } from 'react-redux';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
-import { Error, Loader, SongCard } from '../components';
-import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
+import { Error, Loader, SongCard } from "../components";
+import { useGetSongsByCountryQuery } from "../redux/services/shazamCore";
 
-const AroundYou = () => {
-  const [country, setCountry] = useState('');
+const CountryTracks = () => {
+  const [country, setCountry] = useState("");
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
   useEffect(() => {
-    axios.get('https://geo.ipify.org/api/v2/country?apiKey=at_OPUx1i81h8Si5qjIEdcbGZB7BAPvu')
+    axios
+      .get(
+        "https://geo.ipify.org/api/v2/country?apiKey=at_OPUx1i81h8Si5qjIEdcbGZB7BAPvu"
+      )
       .then((res) => setCountry(res?.data?.location?.country))
       // eslint-disable-next-line no-console
       .catch((err) => console.log(err))
       .finally(() => setLoading(false));
   }, [country]);
 
-  if (isFetching && loading) return <Loader title="Loading songs around you..." />;
+  if (isFetching && loading)
+    return <Loader title="Loading songs around you..." />;
 
   if (error && country) return <Error />;
 
   return (
     <div className="flex flex-col">
-      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Around You
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Around You
         <span className="font-black">{country}</span>
       </h2>
 
@@ -45,4 +50,4 @@ const AroundYou = () => {
   );
 };
 
-export default AroundYou;
+export default CountryTracks;
